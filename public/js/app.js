c8b70dacac12300fd7fe17f2b34bd0a792f2c4a7
levxyca <leticiacaroline-123@hotmail.com>
2021-12-10 15:50:31 -0300

    :lipstick: navegação entre as abas de buscar e registrar posts

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+const btnSearch = document.getElementById("app_btn-search");
+const btnRegister = document.getElementById("app_btn-register");
+
+let content = document.getElementById("app__pesquisa");
+let respostaErroApp = document.getElementById("app__erro");
+let resultado = document.getElementById("app__resultado");
+let title = document.getElementById("app__title");
+let link = document.getElementById("app__link");
+let respostaErroAppRegistro = document.getElementById("app__erro-register");
+
+btnSearch.addEventListener("click", () => {
+  content = content.value;
+
+  if (content == "") {
+    respostaErroApp.innerHTML = "Você precisa inserir pelo menos 1 caractere!";
+  } else {
+    console.log("tudo ok");
+  }
+});
+
+btnRegister.addEventListener("click", () => {
+  title = title.value;
+  link = link.value;
+
+  if (title == "" || link == "") {
+    respostaErroAppRegistro.innerHTML =
+      "Você precisa inserir pelo menos 1 caractere em cada campo!";
+  } else {
+    console.log("ok");
+  }
+});
