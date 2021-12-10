d88b8b34d1d48530415175c262926155eb0e0f72
levxyca <leticiacaroline-123@hotmail.com>
2021-12-10 16:57:34 -0300

    :sparkles: login utilizando cookies

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cookieParser = require("cookie-parser");
+
 let http = require("http"),
   path = require("path"),
   express = require("express"),
@@ -8,15 +10,34 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  if (req.cookies && req.cookies.login) {
+    return res.redirect("/posts");
+  }
+  return res.render("index");
+});
+
+app.post("/login", (req, res, next) => {
+  let login = req.body.user;
+  let password = req.body.pass;
+  console.log(login, password);
+  if (login === "leticia" && password === "123") {
+    res.cookie("login", "leticia");
+    return res.redirect("/posts");
+  } else {
+    return res.redirect("/#modal__login");
+  }
 });
 
 app.get("/posts", async (req, res) => {
-  const busca = req.query.busca;
-  const posts = await Posts.find(busca);
-  res.render("posts", { posts: posts });
+  if (req.cookies && req.cookies.login) {
+    const busca = req.query.busca;
+    const posts = await Posts.find(busca);
+    return res.render("posts", { posts: posts });
+  }
+  return res.redirect("/#modal__login");
 });
 
 app.post("/posts", async (req, res) => {
@@ -24,7 +45,7 @@ app.post("/posts", async (req, res) => {
   const description = req.body.description;
   const link = req.body.link;
   Posts.insert(title, description, link);
-  res.redirect("/posts");
+  return res.redirect("/posts");
 });
 
 const port = 3000;
--- public/js/login.js
+++ public/js/login.js
@@ -16,35 +16,5 @@ btnModalLogin.addEventListener("click", () => {
   } else if (login.email.length < 3 || login.password.length < 3) {
     respostaErroLogin.innerHTML = "Você precisa inserir mais de 3 dígitos!";
   } else {
-    var ajax = new XMLHttpRequest();
-
-    ajax.open("POST", "https://reqres.in/api/login", true);
-    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    ajax.send(`email=${login.email}&password=${login.password}`);
-
-    ajax.onreadystatechange = function () {
-      if (ajax.status == 200) {
-        localStorage.setItem("logged", true);
-        localStorage.setItem("token", ajax.responseText);
-
-        const title = document.querySelector(".modal__login-title");
-        const titleSucesso = document.querySelector(
-          ".modal__login-title-sucesso"
-        );
-        const form = document.querySelector(".modal__login-form");
-        const home = document.querySelector(".home");
-        const app = document.querySelector(".app");
-
-        respostaErroLogin.classList.toggle("none");
-        form.classList.toggle("none");
-        title.classList.toggle("none");
-        titleSucesso.classList.toggle("none");
-        home.classList.toggle("none");
-        app.classList.toggle("none");
-      } else {
-        respostaErroLogin.innerHTML =
-          "Desculpe, ocorreu um erro ao tentar realizar o seu login.";
-      }
-    };
   }
 });
